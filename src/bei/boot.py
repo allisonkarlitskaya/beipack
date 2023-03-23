c61f2bf20781b2e0fbef413e9a0c164175e3d3af
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-03-23 16:39:09 +0100

    beiboot: make --script an optional argument
    This turns the old:
    
      beiboot script.py subcommand ...
    
    into:
    
      beiboot --script script.py subcommand ...
    
    which is roughly equivalent to:
    
      beiboot subcommand ... < script.py
    
    And allows a new possibility:
    
      beiboot subcommand ...
    
    which simply starts an interactive Python interpreter in a given
    context, such as:
    
      beiboot ssh c
    
    Additionally, add some smarts to recognise 'ssh' as a command name and
    perform some adjustments to its arguments to account for the fact that
    it expects a shell script, not an argument vector.  Also add 'container'
    as a useful alias to start an interpreter via podman exec.
    
    Finally, if run with no positional parameters, beiboot will just start
    the interpreter in the local context.  This means that:
    
      beiboot
    
    is roughly equivalent to:
    
      python -iq
    
    But when piping a script:
    
      cat script.py | beiboot
    
    avoids the repeated printing of >>> and ... lines.
    
    The most interesting possibility introduced by this new interface is
    that it's now possible to do things like execute beipacks created on the
    fly:
    
      beipack ... | beiboot ssh c
    
    Note: Somewhat annoyingly, the Python REPL appears to print an extra
    newline on exit due to EOF, and I haven't been able to find a way to
    disable that.  Scripts can avoid it by explicitly calling sys.exit().

--- src/bei/boot.py
+++ src/bei/boot.py
@@ -19,23 +19,92 @@
 import argparse
 import subprocess
 import os
+import shlex
 import sys
 
-from typing import Optional
+from typing import Sequence
+
+
+def get_python_command(local=False, tty=False, sh=False) -> Sequence[str]:
+    interpreter = sys.executable if local else 'python3'
+    command: Sequence[str]
+
+    if tty:
+        command = (interpreter, '-iq')
+    else:
+        command = (
+            interpreter, '-ic',
+            # https://github.com/python/cpython/issues/93139
+            '''" - beiboot - "; import sys; sys.ps1 = ''; sys.ps2 = '';'''
+        )
+
+    if sh:
+        command = (' '.join(shlex.quote(arg) for arg in command),)
+
+    return command
+
+
+def get_ssh_command(*args, tty=False) -> Sequence[str]:
+    return ('ssh',
+            *(['-t'] if tty else ()),
+            *args,
+            *get_python_command(tty=tty, sh=True))
+
+
+def get_container_command(*args, tty=False) -> Sequence[str]:
+    return ('podman', 'exec', '--interactive',
+            *(['--tty'] if tty else ()),
+            *args,
+            *get_python_command(tty=tty))
+
+
+def get_local_command(tty=False) -> Sequence[str]:
+    return get_python_command(local=True, tty=tty)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('script',
+    parser.add_argument('--sh', action='store_true',
+                        help='Pass Python interpreter command as shell-script')
+    parser.add_argument('--script',
                         help="the script to run remotely (must be repl-friendly)")
-    parser.add_argument('command', nargs='*',
-                        help="the command to connect to the remote (like 'ssh host' or 'flatpak-spawn --host')")
-    args = parser.parse_args()
+    parser.add_argument('command', nargs='*')
 
+    """
+    subparsers = parser.add_subparsers(title='Connection type')
+
+    ssh_parser = subparsers.add_parser('ssh', help='Connect via ssh')
+    ssh_parser.add_argument('args', nargs='+')
+    ssh_parser.set_defaults(get_command=get_ssh_command)
+
+    container_parser = subparsers.add_parser('container', help='Run in a (running) container')
+    container_parser.add_argument('args', nargs='+')
+    container_parser.set_defaults(get_command=get_container_command)
+
+    parser.set_defaults(get_command=get_local_command, args=[])
+    """
+
+    args = parser.parse_args()
+    tty = not args.script and os.isatty(0)
+
+    if args.command == []:
+        command = get_local_command(tty=tty)
+    elif args.command[0] == 'ssh':
+        command = get_ssh_command(*args.command[1:], tty=tty)
+    elif args.command[0] == 'container':
+        command = get_container_command(*args.command[1:], tty=tty)
+    else:
+        command = get_python_command(*args.command, tty=tty, sh=args.sh)
+
+    # If we're streaming from stdin then this is a lot easier...
+    if not args.script:
+        os.execlp(command[0], *command)
+
+    # Otherwise, "full strength"
     with open(args.script, 'rb') as file:
         script = file.read()
 
-    with subprocess.Popen([*args.command, 'python3', '-i'], stdin=subprocess.PIPE) as proc:
+    with subprocess.Popen(command, stdin=subprocess.PIPE) as proc:
         assert proc.stdin is not None
         proc.stdin.write(script)
 
