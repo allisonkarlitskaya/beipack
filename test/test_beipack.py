5adf1920a642c35e2b2d5b8a5c291702ce2f4502
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-05-24 11:14:37 +0200

    beipack: up our ResourceReader game
    Implement a full-featured ResourceReader.

--- test/test_beipack.py
+++ test/test_beipack.py
@@ -68,14 +68,21 @@ def test_resources(python_command: list[str],
                    pytestconfig: pytest.Config) -> None:
     pack = beipack.pack(
         {
+            'x/subdir/__init__.py': b'',
             'x/__init__.py': b'',
             'x/y.py':
                 b'# can you hear me now?\n' +
                 b'from importlib import resources\n' +
                 b'def main():\n'
                 b'    print((resources.files("x") / "y.py").read_text())\n'
+                b'    print([item.name for item in resources.files("x").iterdir()])\n'
         },
         entrypoint='x.y:main'
     )
     print(pack)
-    assert 'can you hear me now' in run_pack(pack)
+    result = run_pack(pack)
+    print(result)
+    assert 'can you hear me now' in result
+    assert "'__init__.py'" in result
+    assert "'subdir'" in result
+    assert "'y.py'" in result
